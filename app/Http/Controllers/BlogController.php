dd52277cfcb2c9f981cd12cdf035266c75e5996e
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-07-08 10:23:49 +0300

    get blog with categories

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -16,8 +16,9 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $blog = Blog::all();
-        return response()->json($blog);
+        $blog = Blog::query();
+
+        return response()->json($blog->get()->load('categories', 'user'));
     }
 
     /**
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,10 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
-
     protected $fillable = [
         'title',
         'description',
         'user_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasManyThrough(Category::class, BlogCategories::class, 'category_id', 'id', 'id', 'blog_id');
+    }
+
+
 }
